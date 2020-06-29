1a5b1ad99db022f7b1e884bcad8c0b1138b37947
龍門渕透華 <swearl@163.com>
2020-06-29 18:05:55 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app> </v-app>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style lang="scss">
+#app {
+    font-family: 'PingFang SC', 'Hiragino Sans GB', 'Heiti SC',
+        'Microsoft YaHei', 'WenQuanYi Micro Hei', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+import vuetify from './plugins/vuetify'
+
+Vue.config.productionTip = false
+
+// 自动注册全局组件
+const requireComponent = require.context('./components', true, /index\.vue$/)
+requireComponent.keys().forEach(fileName => {
+    const componentConfig = requireComponent(fileName),
+        componentName = componentConfig.default.name
+    Vue.component(componentName, componentConfig.default || componentConfig)
+})
+
+new Vue({
+    store,
+    vuetify,
+    render: h => h(App)
+}).$mount('#app')
