4b0dc77828b5849a23eda72a4182278af90e3f43
龍門渕透華 <swearl@163.com>
2020-07-02 16:43:41 +0800

    分离window组件

--- src/components/LoginForm/index.vue
+++ src/components/LoginForm/index.vue
@@ -1,18 +1,13 @@
 <template>
     <v-row justify="center">
         <v-dialog persistent max-width="250" v-model="loginFormShow">
-            <v-card>
-                <v-system-bar>
-                    <span>登录</span>
-                </v-system-bar>
-                <v-card-text>
-                    <v-text-field
-                        label="昵称"
-                        v-model="nickname"
-                        @keyup.enter="setNickname"
-                    />
-                </v-card-text>
-            </v-card>
+            <window title="登录">
+                <v-text-field
+                    label="昵称"
+                    v-model="nickname"
+                    @keyup.enter="setNickname"
+                />
+            </window>
         </v-dialog>
     </v-row>
 </template>
---
+++ src/components/Window/index.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-card class="window">
+        <v-system-bar>
+            <span class="window-title" v-if="title != ''" v-html="title" />
+            <v-spacer v-if="btnClose" />
+            <v-btn v-if="btnClose" x-small icon>
+                <v-icon>{{ icons.mdiClose }}</v-icon>
+            </v-btn>
+        </v-system-bar>
+        <v-card-text>
+            <slot />
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+import { mdiClose } from '@mdi/js'
+
+export default {
+    name: 'Window',
+    props: {
+        icon: {
+            type: String,
+            default: ''
+        },
+        title: {
+            type: String,
+            default: ''
+        },
+        btnClose: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            icons: { mdiClose }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.v-system-bar {
+    .v-btn {
+        .v-icon {
+            margin-right: 0;
+        }
+    }
+}
+</style>
+
+<style lang="scss" scoped>
+.window {
+    .window-title {
+        cursor: default;
+        user-select: none;
+    }
+}
+</style>
