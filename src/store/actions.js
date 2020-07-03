513af30ae0402a1b472cf424d2449b9aa835e769
龍門渕透華 <swearl@163.com>
2020-07-03 11:39:12 +0800

    优化初始交互 添加环境变量例子文件

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,7 +3,9 @@ const actions = {
         if (state.user.nickname == '') {
             dispatch('loginForm/show')
         } else {
-            dispatch('desktop/show')
+            dispatch('loading/show', { text: '登录中', time: 3 }).then(() => {
+                dispatch('desktop/show')
+            })
         }
     }
 }
