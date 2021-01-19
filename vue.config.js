bdadfff7b1168dfc435b3cf862f0af2e216bd4c9
龍門渕透華 <swearl@163.com>
2021-01-19 17:50:09 +0800

    又踩完坑了, 重写v4, 换回vue2+vuetify

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  transpileDependencies: ['vuetify'],
+  pwa: {
+    name: process.env.APP_TITLE,
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'default',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png',
+    },
+  },
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = process.env.APP_TITLE;
+      return args;
+    });
+  },
+  productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
+  devServer: {
+    port: process.env.DEV_PORT,
+    // proxy: {
+    //   '/api': {
+    //     target: process.env.SOCKET_SERVER,
+    //     changeOrigin: true,
+    //   },
+    // },
+  },
+};
